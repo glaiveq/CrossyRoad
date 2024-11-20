20b6d2c6f987fc90b0ca1390d002987d79e76c85
glaiveq <tirtirovich@gmail.com>
2024-11-20 18:32:10 +0200

    Initial commit

---
+++ Source/CrossyRoad/Public/CRGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CRGameMode.generated.h"
+
+UCLASS()
+class CROSSYROAD_API ACRGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CrossyRoad/Public/CRPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CRPlayerController.generated.h"
+
+UCLASS()
+class CROSSYROAD_API ACRPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
